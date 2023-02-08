24cc1811c348a80a705933b1b3b232221f3d2511
0131ryu <131ryuji@gmail.com>
2023-02-08 23:35:56 +0900

    원티드 과제 - section2

---
+++ src/todo/index.js
@@ -0,0 +1,12 @@
+"use strict";
+let TodoList;
+//할 일 추가(내용 필수)
+const createTodo = (content) => { };
+//모든 할 일 조회(id 기반으로 특정 할 일 조회)
+const readTodo = (id) => { };
+//update(id 제외한 모든 속성 수정, 특정 할 일의 특정 태그 수정 가능)
+const updateTodo = (id, tags) => { };
+//delete(id기반 특정 할 일 삭제 가능, 모든 할 일, 특정 할 일의 특정 태그나 모든 태그 제거 가능)
+const deleteTodo = (id, tags) => { };
+const allDeleteTag = (id) => { };
+const allDeleteContent = (id) => { };
